c792bfb93e55d77c4eb5cdf428d6a2a106389db1
fred d kim <yhk1038@hanmail.com>
2017-05-03 19:22:21 +0900

    update meta tags

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,6 +3,24 @@ module ApplicationHelper
         'HereFan'
     end
     
+    def app_description
+        'The fastest way to k-dol stars, HereFan'
+    end
+    
+    def app_domain
+        'http://13.124.91.0'
+    end
+    
+    def app_config
+        app_config_info = {}
+        app_config_info[:title]         = app_title
+        app_config_info[:description]   = app_description
+        app_config_info[:url]           = app_domain
+        app_config_info[:image]         = app_domain + '/svg/main_logo.svg'
+        
+        return app_config_info
+    end
+    
     def now
         DateTime.now
     end
