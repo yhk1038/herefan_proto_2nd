474ac8b08b2a1985c37e38dbe5e571a3e7265ae5
fred d kim <yhk1038@hanmail.com>
2017-05-08 04:16:26 +0900

    update2 admin dashboard ~> rollincode (theme)

--- config/application.rb
+++ config/application.rb
@@ -6,6 +6,9 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
+# RAIlS_ADMIN_THEME environment variable configuration
+ENV['RAILS_ADMIN_THEME'] = 'rollincode'
+
 module HerefanLink
     class Application < Rails::Application
         # Settings in config/environments/* take precedence over those specified here.
