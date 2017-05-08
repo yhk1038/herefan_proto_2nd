06c0d52cd5782a69d4201c62d04f4473ce389cfe
fred d kim <yhk1038@hanmail.com>
2017-05-08 13:59:41 +0900

    ah2....

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,6 +1,5 @@
 class ErrorsController < ApplicationController
     before_action :error_info
-    layout '/errors/layout'
     
     def show
         render "/errors/#{status_code.to_s}", status: status_code
