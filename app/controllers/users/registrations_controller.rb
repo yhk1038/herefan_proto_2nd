3f8b8240ff1aa7ce8e04cd18b3bc7fe69e0a8e60
fred d kim <yhk1038@hanmail.com>
2017-05-15 16:38:11 +0900

    Bug Fix :: mypage

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,6 +1,7 @@
 class Users::RegistrationsController < Devise::RegistrationsController
     before_action :configure_sign_up_params, only: [:create]
     before_action :configure_account_update_params, only: [:update]
+    before_action :my_published_fandoms
     
     # GET /resource/sign_up
     # def new
