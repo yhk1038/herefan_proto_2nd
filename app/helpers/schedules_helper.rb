1d94dd51123541ab61512b49b72e65c0005b693d
fred d kim <yhk1038@hanmail.com>
2017-05-17 14:47:52 +0900

    Fix calendar header image reorder

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -2,8 +2,9 @@ module SchedulesHelper
     
     def find_celandar_header_image(schedules)
         found = false
-        schedules.order(event_end: :desc).each do |schedule|
+        schedules.order(event_start: :asc).each do |schedule|
             next if schedule.content.nil?
+            # next if schedule.content
             found = schedule.content
         end
         found ? found : @fandom.background_img
