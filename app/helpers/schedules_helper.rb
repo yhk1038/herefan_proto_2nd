d27505dcf8d5e4615a5abc1a633a062de2e67e29
fred d kim <yhk1038@hanmail.com>
2017-05-30 19:03:11 +0900

    Bug Fix :: Schedule image header

--- app/helpers/schedules_helper.rb
+++ app/helpers/schedules_helper.rb
@@ -3,7 +3,7 @@ module SchedulesHelper
     def find_celandar_header_image(schedules)
         found = false
         schedules.order(event_start: :asc).each do |schedule|
-            next if schedule.content.nil?
+            next if schedule.content.nil? || schedule.content.length < 2
             # next if schedule.content
             found = schedule.content
         end
