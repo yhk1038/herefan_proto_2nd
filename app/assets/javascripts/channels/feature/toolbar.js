f3a48492137be1e2df3ac6543e9bf68c6a1a344c
fred d kim <yhk1038@hanmail.com>
2017-06-16 15:22:10 +0900

    sharing bug fix

--- app/assets/javascripts/channels/feature/toolbar.js
+++ app/assets/javascripts/channels/feature/toolbar.js
@@ -101,5 +101,117 @@ function getMyCards(_req) {
             width: 250
         });
         $('#library').attr('style', '');
+        bindingActions();
+    });
+}
+
+
+function bindingActions() {
+    // 링크 카드 Like 기능
+    $('.actions_box .maum').unbind('click').click(function (event) {
+        var btn     = $(this);
+        var user_id = $(this).attr('action_noiser');
+        var target_id = $(this).attr('action_target');
+        var method  = $(this).attr('action_status');
+        var url     = '';
+
+        if (user_id !== '0'){
+            if (method === 'post'){
+                url += '/action/likes.json'
+            } else if (method === 'delete'){
+                url += '/action/likes/'+target_id+'.json'
+            }
+
+            var req = $.ajax({
+                url: url,
+                method: method,
+                data: {
+                    like: {
+                        user_id: user_id,
+                        link_id: target_id
+                    },
+                    authenticity_token: _hf_
+                }
+            });
+
+            req.done(function (result) {
+                console.log(result);
+                var icon = $('#actions_'+result.data.link_id+' .maum span.zmdi');
+                var counter = $('#actions_'+result.data.link_id+' .maum span.counter');
+
+                if (result.status === 'created'){
+                    icon.removeClass('zmdi-favorite-outline').addClass('zmdi-favorite');
+                    counter.text(result.count);
+                    btn
+                        .attr('action_status', 'delete')
+                        .attr('action_target', result.data.id);
+
+                } else if (result.status === 'deleted'){
+                    icon.removeClass('zmdi-favorite').addClass('zmdi-favorite-outline');
+                    counter.text(result.count);
+                    btn
+                        .attr('action_status', 'post')
+                        .attr('action_target', result.data.link_id);
+                }
+            });
+
+            req.fail(function (result) { console.log('failed'); alert(result.responseText) })
+
+        } else {
+            alert('you should login to use like button')
+        }
+
+        event.stopPropagation();
+    });
+    // 링크 카드 Clip 기능
+    $('.clip_btn').unbind('click').click(function (event) {
+        var btn = $(this);
+        var user_id = $(this).attr('action_noiser');
+        var target_id = $(this).attr('action_target');
+        var method  = $(this).attr('action_status');
+        var url     = '';
+
+        if (user_id !== '0'){
+            if (method === 'post'){
+                url += '/action/clips.json'
+            } else if (method === 'delete'){
+                url += '/action/clips/'+target_id+'.json'
+            }
+
+            var req = $.ajax({
+                url: url,
+                method: method,
+                data: {
+                    clip: {
+                        user_id: user_id,
+                        link_id: target_id
+                    },
+                    authenticity_token: _hf_
+                }
+            });
+
+            req.done(function (result) {
+                console.log(result);
+                var grand = $('#link_'+result.data.link_id);
+
+                if (result.status === 'created'){
+                    grand.addClass('clipped');
+                    btn
+                        .attr('action_status', 'delete')
+                        .attr('action_target', result.data.id);
+
+                } else if (result.status === 'deleted'){
+                    grand.removeClass('clipped');
+                    btn
+                        .attr('action_status', 'post')
+                        .attr('action_target', result.data.link_id);
+                }
+            });
+
+            req.fail(function (result) { console.log('failed'); hey_login() })
+
+        }
+
+        event.stopPropagation()
     });
 }
