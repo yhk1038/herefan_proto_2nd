f3b608abf2ad4738bcbd2c4cfe26bcca21e7f947
EC2 Default User <ec2-user@ip-172-31-8-235.ap-northeast-2.compute.internal>
2017-06-17 18:24:36 +0000

    121

--- db/schema.rb
+++ db/schema.rb
@@ -128,8 +128,9 @@ ActiveRecord::Schema.define(version: 20170616081725) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.datetime "created_at",                                   null: false
-    t.datetime "updated_at",                                   null: false
+    t.datetime "created_at",                                                     null: false
+    t.datetime "updated_at",                                                     null: false
+    t.integer  "fd_conf_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["fd_conf_id"], name: "index_users_on_fd_conf_id"
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
