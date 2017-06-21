995ddef5e69ed2c98c62f85922f93579b1cf9e2b
fred d kim <yhk1038@hanmail.com>
2017-06-21 19:18:54 +0900

    GA & Wiki Update

--- app/assets/javascripts/channels/feature/wiki_post.js
+++ app/assets/javascripts/channels/feature/wiki_post.js
@@ -25,14 +25,55 @@ $(document).ready(function () {
 
     // Editor
     // ready for saving textarea by taking Editor's value
-    $('.note-editable.panel-body').keyup(function () {
-        $('#wiki_post_content').text($(this).html());
+    $('.note-editable.panel-body').keyup(function (e) {
+        var html = $(this).html();
+
+        var code = e.keyCode || e.which;
+        if(code === 9){
+            html = autoImage(html);
+            console.log(html);
+            $(this).html(html);
+        }
+
+        $('#wiki_post_content').text(html);
     });
     $('.note-editable.panel-body').mouseup(function () {
         $('#wiki_post_content').text($(this).html());
+    });
+    $(this).on('drop', function (e) {
+        // console.log(e);
+        return false;
     });
 
     // Only for Bug Fix on production server
     $('#wiki-ground[data-page="new"] #wiki_post_wiki_pointer_id').removeAttr('value');
 });
 
+function autoImage(html) {
+    console.log('/// start');
+    console.log('');
+    var reg = /(.|\s)h(ttp:\/\/.|ttps:\/\/.)\S*?\.(png|gif|jpg|jpeg)/g;
+    var urls = html.match(reg);
+    var editor = $('.note-editable.panel-body');
+    var content = editor.html();
+
+
+    $.map(urls, function (url, i) {
+        var u = '';
+
+        if (url.indexOf('"') === 0){
+            u = url
+        } else {
+            var ur = url.trim().slice(1);
+            var f_ = url.trim().charAt(0);
+            u = '<img style="max-width: 600px; height: auto;" src="'+ ur +'">';
+            var str = f_ + u;
+            console.log(i+'_str: ', str);
+            console.log('');
+            console.log('end //');
+            content = content.replace(url, str);
+        }
+    });
+
+    return content
+}
